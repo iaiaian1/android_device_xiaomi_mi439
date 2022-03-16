57385520a885d6b46059fdd9caf0e28f0cc67aa9
Jake <yapjakebrian1@gmail.com>
2022-03-16 11:18:37 +0800

    Initial XPE device tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mi439.mk
+    $(LOCAL_DIR)/xperience_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mi439-user \
-    lineage_mi439-userdebug \
-    lineage_mi439-eng
+    xperience_mi439-user \
+    xperience_mi439-userdebug \
+    xperience_mi439-eng
