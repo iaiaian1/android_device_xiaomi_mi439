98bcc57c11dee607cec37ff8c2c9460b0c785171
Jake <yapjakebrian1@gmail.com>
2022-04-01 18:16:18 +0800

    Remove inherits now

--- device.mk
+++ device.mk
@@ -106,9 +106,7 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    vendor/qcom/opensource/commonsys-intf/display/libqdmetadata \
-    vendor/qcom/opensource/commonsys-intf/display/aidl/config
+    $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/mi439/mi439-vendor.mk)
