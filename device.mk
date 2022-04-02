3cd6db7471ad05b16fb22b7c4987418b8b02a769
Jake <yapjakebrian1@gmail.com>
2022-04-02 16:02:30 +0800

    temp: inherit libqdmetadata from intf

--- device.mk
+++ device.mk
@@ -107,7 +107,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    vendor/qcom/opensource/commonsys/display
+    vendor/qcom/opensource/commonsys/display \
+    vendor/qcom/opensource/commonsys-intf/display/libqdmetadata
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/mi439/mi439-vendor.mk)
