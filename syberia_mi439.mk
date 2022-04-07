abc4e01d9c2ccb780867fed78515b5ec67b9a129
Jake <yapjakebrian1@gmail.com>
2022-04-07 14:14:15 +0800

    Revert "Update fingerprint override logic"
    This reverts commit cb926effb47a3028f6ec675c5e210ba720ab4b8f.

--- syberia_mi439.mk
+++ syberia_mi439.mk
@@ -37,12 +37,8 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_MAINTAINER := iaiaian1
 
-# Fingerprint
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys
-BUILD_DESCRIPTION := raven-user 12 SQ1D.211205.016.A1 7957957 release-keys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
