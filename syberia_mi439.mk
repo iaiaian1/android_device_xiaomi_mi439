33f079bf460a152810ad91629f1cec54c60349f2
Jake <yapjakebrian1@gmail.com>
2022-04-07 14:00:57 +0800

    Update raven fingerprint to april
    Signed-off-by: Jake <yapjakebrian1@gmail.com>

--- syberia_mi439.mk
+++ syberia_mi439.mk
@@ -38,7 +38,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_MAINTAINER := iaiaian1
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
